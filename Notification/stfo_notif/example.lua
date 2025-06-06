ae4506332448d639ff99e02d1d3f0787909d23c1
jengu <124980843+jjengu@users.noreply.github.com>
2025-06-06 02:02:02 -0700

    Create example.lua

---
+++ Notification/stfo_notif/example.lua
@@ -0,0 +1,9 @@
+local Notify = loadstring(game:HttpGet("https://pastebin.com/raw/uW8sGTpF"))()
+
+Notify({
+    Message = "stfo notifcation ui ported by @_jengu!",
+    BackgroundColor = Color3.fromRGB(168, 235, 113),    -- https://rgbcolorpicker.com
+    TextColor = Color3.fromRGB(255, 255, 255), 
+    TextFont = Enum.Font.SourceSansBold, -- get enum codes at https://create.roblox.com/docs/reference/engine/enums/Font
+    Duration = 5
+})
