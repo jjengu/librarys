fa3125c0e62be35162963f0fa2a48465de721fe6
jengu <124980843+jjengu@users.noreply.github.com>
2025-10-21 01:48:37 -0400

    Update src.lua

--- millenium/src.lua
+++ millenium/src.lua
@@ -1996,7 +1996,7 @@
                         FontFace = fonts.small;
                         TextColor3 = rgb(245, 245, 245);
                         BorderColor3 = rgb(0, 0, 0);
-                        Text = name;
+                        Text = cfg.name;
                         Parent = items[ "dropdown_object" ];
                         Name = "\0";
                         Size = dim2(1, 0, 0, 0);
